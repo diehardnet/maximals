b41d4bccaa47ce4eec5de3a055e4dd86a6fa10cb
fernandoFernandeSantos <fernandofernandesantos@gmail.com>
2023-12-10 09:44:34 +0000

    Parsers for ChipIR december 2023 test.

--- parsers/common.py
+++ parsers/common.py
@@ -5,38 +5,65 @@ import re
 from typing import List, Tuple, Union
 
 
-def search_header(lines: List[str], log) -> Tuple[Union[str, None], Union[int, None]]:
+def search_header(lines: List[str], log) -> Tuple[float, Union[str, None], Union[int, None]]:
     for line in lines:
         m = re.match(r".*config=.+/(\S+)\.yaml.*batch_size=(\d+).*", line)
         if m:
-            return m.group(1), int(m.group(2))
+            raise ValueError(line)
+            # return m.group(1), int(m.group(2))
 
         m = re.match(r"#HEADER.*dataset: iterations=.*goldpath=.+/(\S+).pt .*usetorchcompile=False", line)
         if m:
-            return m.group(1), 1
+            raise ValueError(line)
 
-        m = re.match(r"#HEADER.*model=(\S+) batchsize=(\d+).*", line)
+            # return m.group(1), 1
+
+        m = re.match(r"#HEADER.*[error|float]_threshold:(\S+) .*model=(\S+) batchsize=(\d+).*", line)
         if m:
-            return m.group(1), int(m.group(2))
+
+            return float(m.group(1)), m.group(2), int(m.group(3))
 
     for line in lines:
         m = re.match(r".*config.+/(\S+)\.yaml.*", line)
         if m:
-            return m.group(1), -1
+            raise ValueError(line)
+
+            # return m.group(1), -1
     for line in lines:
         m = re.match(r"#SERVER_HEADE.*--batchsize (\d+).*--model (\S+) {2}--disableconsolelog", line)
         if m:
-            return m.group(2), m.group(1)
+            raise ValueError(line)
 
-    return None, None
+            # return m.group(2), m.group(1)
+
+    raise ValueError(f"Not a correct log:{log}")
+
+
+def get_hardening_type_from_header(lines: list) -> bool:
+    hardened_id_found = False
+    for line in lines:
+        if "hardenedid" in line:
+            hardened_id_found = True
+        m = re.match(r'#HEADER.*hardenedid=([False|True]+).*', line)
+        if m:
+            if m.group(1) == "False":
+                return False
+            else:
+                return True
+    if hardened_id_found:
+        raise ValueError("Header not found")
+    return False
 
 
 def search_error_criticality(err_str: str) -> re.Match:
-    # diehardnet
-    m = re.match(r"#ERR batch:\d+ critical-img:\d+ i:\d+ g:(\d+) o:(\d+) gt:(\d+)", err_str)
-    # Maximals
-    if m is None:
-        m = re.match(r"#ERR.*critical.*g:(\d+) o:(\d+) gt:(\d+)", err_str)
+    # # diehardnet
+    # m = re.match(r"#ERR batch:\d+ critical-img:\d+ i:\d+ g:(\d+) o:(\d+) gt:(\d+)", err_str)
+    # # Maximals
+    # if m is None:
+    #
+    m = re.match(r"#ERR.*critical.*g:(\d+) o:(\d+) gt:(\d+)", err_str)
+    if not m:
+        m = re.match(r"#ERR.*critical.*glb:(\d+) flb:(\d+)", err_str)
     return m
 
 
@@ -52,22 +79,25 @@ def parse_log_file(log_path: str) -> List[dict]:
 
         with open(log_path) as log_fp:
             lines = log_fp.readlines()
-
-        config, batch_size = search_header(lines=lines, log=log_path)
-        if config is None or batch_size is None or config == "urations":
-            raise ValueError(f"Problem on parsing header {log_path}")
+        if len(lines) <= 3:
+            return []
+        sdc_threshold, config, batch_size = search_header(lines=lines, log=log_path)
+        hardened_id_enable = get_hardening_type_from_header(lines=lines)
+        # if config is None or batch_size is None or config == "urations":
+        #     raise ValueError(f"Problem on parsing header {log_path} {config, batch_size}")
         data_dict = dict(start_dt=start_dt, config=config, ecc=ecc, hostname=hostname,
-                         logfile=os.path.basename(log_path), batch_size=batch_size)
+                         logfile=os.path.basename(log_path), batch_size=batch_size, hardened_id=hardened_id_enable,
+                         sdc_threshold=sdc_threshold)
 
         last_acc_time = 0
-        critical_sdc, evil_sdc, benign_sdc = 0, 0, 0
+        critical_sdc = 0
         for line in lines:
             ct_m = search_error_criticality(err_str=line)
             if ct_m:
                 critical_sdc += 1
-                golden, output, ground_truth = ct_m.group(1), ct_m.group(2), ct_m.group(3)
-                evil_sdc += int(output != ground_truth)
-                benign_sdc += int(output == ground_truth and golden != ground_truth)
+                # golden, output = ct_m.group(1), ct_m.group(2)
+                # evil_sdc += int(output != ground_truth)
+                # benign_sdc += int(output == ground_truth and golden != ground_truth)
             elif "critical" in line:
                 raise ValueError(f"Not a valid line {line}")
 
@@ -78,11 +108,10 @@ def parse_log_file(log_path: str) -> List[dict]:
                 curr_data = copy.deepcopy(data_dict)
                 curr_data.update(
                     dict(it=it, ker_time=float(ker_time), acc_time=0, ker_err=ker_err, acc_err=acc_err, sdc=1,
-                         critical_sdc=int(critical_sdc != 0), evil_sdc=evil_sdc, benign_sdc=benign_sdc,
-                         hostname=hostname)
+                         critical_sdc=int(critical_sdc != 0), hostname=hostname)
                 )
                 data_list.append(curr_data)
-                critical_sdc, evil_sdc, benign_sdc = 0, 0, 0
+                critical_sdc = 0
 
         if data_list:
             data_list[-1]["acc_time"] = last_acc_time
--- parsers/parse_errors_diehardnet_rad.py
+++ parsers/parse_errors_diehardnet_rad.py
@@ -22,7 +22,7 @@ def main():
     args = parse_args()
     data_list = list()
     for subdir, dirs, files in os.walk(args.logdir):
-        if any([i in subdir for i in ["carolp", "carolm", "carola"]]):
+        if any([i in subdir for i in ["carolp20002"]]):
             print("Parsing", subdir)
             for file in files:
                 path = os.path.join(subdir, file)
